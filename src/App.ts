7803a5101c87fc8e78e2ebe935edbf9d3955dce4
Cayetano97 <lopezcayetano315@gmail.com>
2023-05-03 16:09:32 +0200

    fix-A

--- src/App.ts
+++ src/App.ts
@@ -1,60 +1,81 @@
-import { Fetch } from "./Fetch";
+import { Fetch, Data } from "./Fetch.js";
 
-interface Task {
-  id: string;
-  title: string;
-  isDone: boolean;
-}
-
+/**
+ * @class App
+ * @description Clase principal de la aplicación
+ * @method init() Inicializa la aplicación
+ * @method generateRow(id, title, done) Genera una fila de la tabla
+ * @method renderTasks(tasks) Renderiza todas las tareas
+ * @method addTask(input, alert) Añade una nueva tarea
+ * @method crossOut(e) Marca o desmarca una tarea realizada
+ * @method editModeOn(e, fromTask) Activa el modo edición
+ * @method editModeOff(e, fromTask) Desactiva el modo edición
+ * @method removeRow(e) Elimina una tarea
+ * @method clearWhitespaces(str) Elimina los espacios en blanco de la tarea en el modo edición
+ * @method idGenerator() Genera un id aleatorio hexadecimal
+ * @property alert Alerta de error
+ * @property close Botón para cerrar la alerta
+ * @property input Input para añadir una nueva tarea
+ * @property arrow Botón para guardar una nueva tarea
+ * @property table Tabla de tareas
+ * @throws Error
+ * @returns {Promise} Promise
+ *
+ */
 export class App {
-  alert :HTMLElement | null; // HTMLElemente quiere decir que puede ser cualquier elemento HTML
-  close: HTMLElement | null;
-  input: HTMLInputElement; // HTMLInputElement: la interfaz para elementos <input>.
-  arrow: HTMLElement | null;
-  table: HTMLTableSectionElement | null; // HTMLTableElement: la interfaz para elementos <table>.
-
+  alert: HTMLDivElement | null;
+  close: HTMLSpanElement | null;
+  input: HTMLInputElement | null;
+  arrow: HTMLDivElement | null;
+  table: HTMLTableSectionElement | null;
   constructor() {
-    this.alert  = document.querySelector(".alert"); // HTMLElement quiere decir que puede ser cualquier elemento HTML
-    this.close  = this.alert?.firstElementChild as HTMLElement; 
-    this.input = document.querySelector("input") as HTMLInputElement;
+    this.alert = document.querySelector(".alert");
+    this.close = this.alert?.firstElementChild as HTMLElement;
+    this.input = document.querySelector("input");
     this.arrow = document.querySelector(".arrow");
     this.table = document.querySelector("tbody");
   }
-
   init = async () => {
     //eventos
     //Cerrar la alerta en el botón con la X
-    if (this.close) {
-    this.close?.addEventListener("click", () => {
-      this.alert?.classList.add("dismissible");
-    });
-  }
+    if (this.close !== null) {
+      this.close.addEventListener("click", () => {
+        this.alert?.classList.remove("dismissible");
+      });
+    }
     //Impedir la recarga de la página y añadir una nueva tarea
-    this.input?.addEventListener("keydown", (e: KeyboardEvent) => {
-      if (e.code == "Enter" || e.code == "NumpadEnter") {
-        e.preventDefault();
-        this.addTask(this.input, this.idGenerator, this.input?.value, this.alert as HTMLElement);
-      }
-    });
-    this.input?.addEventListener("input", (e) => {
-      if (this.input?.value !== "" && !this.alert?.classList.contains("dismissible")) {
-        this.alert?.classList.add("dismissible");
-      }
-    });
-    //Añadir una nueva tarea
-    this.arrow?.addEventListener("click", () => {
-      this.addTask(this.input!, this.idGenerator, this.input?.value, this.alert!);
-    });
+    if (this.input !== null) {
+      this.input.addEventListener("keydown", (e) => {
+        if (e.code == "Enter" || e.code == "NumpadEnter") {
+          e.preventDefault();
+          this.addTask(this.input, this.alert);
+        }
+      });
+      this.input.addEventListener("input", (e) => {
+        if (
+          this.input?.value !== "" &&
+          !this.alert?.classList.contains("dismissible")
+        ) {
+          this.alert?.classList.add("dismissible");
+        }
+      });
+    }
+    if (this.arrow !== null) {
+      //Añadir una nueva tarea
+      this.arrow.addEventListener("click", () => {
+        this.addTask(this.input, this.alert);
+      });
+    }
     // Fetch all tasks
-    let tasks = await Fetch.getAll();
+    let tasks: Data = await Fetch.getAll();
     // Render all tasks
-    this.renderTasks(tasks as unknown as Task[]);
+    this.renderTasks(tasks);
   };
   // //prepara una plantilla HTML, y la actualiza con contenido dinámico
-  generateRow = (id: string, title: string, isDone: boolean) => {
+  generateRow = (id: string, title: string, done: boolean) => {
     let newRow = document.createElement("tr");
     newRow.setAttribute("id", id);
-    title = isDone ? `<del>${title}</del>` : title;
+    title = done ? `<del>${title}</del>` : title;
     newRow.innerHTML = `
 <td>
   <i class="fa-solid fa-circle-check"></i>
@@ -76,72 +97,103 @@ export class App {
     //Tachar una tarea realizada
     newRow.firstElementChild?.firstElementChild?.addEventListener(
       "click",
-      (event: Event) => {
-        this.crossOut(event as MouseEvent);
-      }
+      (e: Event) => this.crossOut(e)
     );
     //Activar el modo edición desde la tarea
-    newRow.firstElementChild?.lastElementChild?.addEventListener("focus", (event: Event) => {
-      this.editModeOn(event as MouseEvent, true);
-    });
+    newRow.firstElementChild?.lastElementChild?.addEventListener(
+      "focus",
+      (e: Event) => {
+        let currentEvent = e as Event & { currentTarget: HTMLElement };
+        this.editModeOn(currentEvent, true);
+      }
+    );
     //Desactivar el modo edición
-    newRow.firstElementChild?.lastElementChild?.addEventListener("blur", (event: Event) => {
-      this.editModeOff(event as MouseEvent);
-    });
+    newRow.firstElementChild?.lastElementChild?.addEventListener(
+      "blur",
+      (e) => {
+        let currentEvent = e as Event & {
+          currentTarget: HTMLElement;
+          target: HTMLElement;
+        };
+        this.editModeOff(currentEvent);
+      }
+    );
     //Activar el modo edición desde el icono
     newRow.firstElementChild?.nextElementSibling?.firstElementChild?.lastElementChild?.addEventListener(
       "click",
-      (event: Event) => {
-        this.editModeOn(event as MouseEvent, false);
+      (e) => {
+        let currentEvent = e as Event & { currentTarget: HTMLElement };
+        this.editModeOn(currentEvent, false);
       }
     );
     //Eliminar la fila
     newRow.lastElementChild?.firstElementChild?.lastElementChild?.addEventListener(
       "click",
-      (event: Event) => {
-        this.removeRow(event as MouseEvent,false);
+      (e) => {
+        let currentEvent = e as Event & { target: HTMLElement };
+        this.removeRow(currentEvent, false);
       }
     );
     return newRow;
   };
-  renderTasks = (tasks: Task[]) => {
-    console.log(tasks.length);
-    tasks.forEach((task: Task) => {
-      this.table?.appendChild(this.generateRow(task.id, task.title, task.isDone));
-    });
+  // Añade todas las tareas a la tabla
+  renderTasks = (tasks: Data) => {
+    if (this.table !== null) {
+      tasks.forEach((task) => {
+        this.table?.appendChild(
+          this.generateRow(task.id, task.title, task.isDone)
+        );
+      });
+    }
   };
   // //Tachado de tarea
-  crossOut = (event: MouseEvent) => {
-    let task = (event.target as HTMLElement)?.nextElementSibling as HTMLElement;
-    let text = task.innerHTML;
-    text = text.includes("<del>") ? task.firstElementChild?.textContent ?? "" : `<del>${text}</del>`;
-    task.innerHTML = text;
-    task.closest('.task-container')?.setAttribute("data-completed", text.includes("<del>") ? "true" : "false");
-  };
-  
-
-  //Añadir nueva tarea
-  addTask = (input: HTMLInputElement, idGenerator: () => string, text: string, alert: HTMLElement): void => {
-    if (!text) {
-      input.value = "";
-      alert.classList.remove("dismissible");
+  crossOut = (e: Event) => {
+    let task: HTMLElement | null = (e.target as HTMLElement)
+      ?.nextElementSibling as HTMLElement;
+    let text: string | null = task?.innerHTML as string | null;
+    const row = (task as HTMLElement)?.parentNode?.parentNode;
+    if (text?.includes("<del>")) {
+      text = task?.firstElementChild?.textContent as string;
+      task.innerHTML = text;
+      // Para validar que existe el elemento, antes de acceder a sus propiedades
+      if (row instanceof HTMLElement) {
+        row.setAttribute("data-completed", "false");
+      }
     } else {
-      text = input.value;
-      const id = idGenerator();
-      document.querySelector("tbody")?.appendChild(this.generateRow(id, text, alert?.classList.contains("dismissible")));
+      task.innerHTML = `<del>${text}</del>`;
+      if (row instanceof HTMLElement) {
+        row.setAttribute("data-completed", "true");
+      }
+    }
+  };
+  //Añadir nueva tarea
+  addTask = (input: HTMLInputElement | null, alert: HTMLDivElement | null) => {
+    let text: string;
+    if (input?.value.trim() === "") {
+      input.value = "";
+      alert?.classList.remove("dismissible");
+    } else if (input !== null) {
+      text = input?.value;
+      document
+        .querySelector("tbody")
+        ?.appendChild(this.generateRow(this.idGenerator(), text, false));
       input.value = "";
     }
   };
-  //Modo Edición
-  editModeOn = (e: Event, onFocus: boolean): void => {
+  //Activar el modo edición
+  editModeOn = (
+    e: Event & { currentTarget: HTMLElement },
+    onFocus: boolean
+  ) => {
     let task: HTMLElement;
     if (onFocus) {
-      task = e.currentTarget as HTMLElement;
+      task = e.currentTarget;
     } else {
-      task = (((((e.currentTarget as HTMLElement).parentNode as HTMLElement).parentNode as HTMLElement).previousElementSibling as HTMLElement).lastElementChild as HTMLElement);
+      task = (
+        (e.currentTarget?.parentNode as HTMLElement)?.parentNode as HTMLElement
+      )?.previousElementSibling?.lastElementChild as HTMLElement;
       task.focus();
     }
-    // console.log(task);(((
     task.classList.add("editable");
     document.addEventListener("keydown", (e) => {
       if (e.code == "Enter" || e.code == "NumpadEnter" || e.code == "Escape") {
@@ -149,8 +201,11 @@ export class App {
       }
     });
   };
-  editModeOff = (e: MouseEvent) => {
-    let task = e.currentTarget as HTMLElement;
+  //Desactivar el modo edición
+  editModeOff = (
+    e: Event & { currentTarget: HTMLElement; target: HTMLElement }
+  ) => {
+    let task = e.currentTarget;
     if (task.innerHTML === "") {
       this.removeRow(e, true);
     } else {
@@ -161,24 +216,25 @@ export class App {
       }
     }
   };
-  // Eliminación de tarea
-   removeRow = (e: Event, editionMode: boolean): void => {
-     if (editionMode) {
-      (e.currentTarget as HTMLElement).closest("tr")?.remove();
+  //Eliminación de tarea
+  removeRow = (e: Event & { target: HTMLElement }, editionMode: boolean) => {
+    if (editionMode) {
+      (
+        (e.target?.parentNode as HTMLElement)?.parentNode as HTMLElement
+      )?.remove();
     } else {
-       // console.log(e.target.parentNode.parentNode.parentNode);
-       (((e.target as HTMLElement).parentNode as HTMLElement).parentNode as HTMLElement).remove();
-      }
-   };
-
-
+      (
+        ((e.target?.parentNode as HTMLElement)?.parentNode as HTMLElement)
+          ?.parentNode as HTMLElement
+      ).remove();
+    }
+  };
   //Eliminación de espacios en blanco
-  clearWhitespaces = (text: string) => {
+  clearWhitespaces = (text: string): string => {
     return text.replace(new RegExp(/&nbsp;/, "g"), "").trim();
   };
-  idGenerator = () => {
+  idGenerator = (): string => {
     // generate random hex string
-     return Math.floor(Math.random() * 16777215).toString(16);
-   }
-
+    return Math.floor(Math.random() * 16777215).toString(16);
+  };
 }
--- src/Fetch.ts
+++ src/Fetch.ts
@@ -1,23 +1,41 @@
-interface Task {
-  id: number;
+/**
+ * @class Fetch
+ * @description Clase para realizar las peticiones a la API
+ * @method getAll() Obtiene todas las tareas
+ * @method create(task) Crea una tarea
+ * @method update(task) Actualiza una tarea
+ * @method delete(id) Elimina una tarea
+ * @property BASE_URL URL base de la API
+ * @static
+ * @throws Error
+ * @returns {Promise} Promise
+ */
+
+export interface ITask {
+  id: string;
   title: string;
   isDone: boolean;
 }
+
+export type Data = ITask[];
+export type PatchTask = Partial<ITask> & { id: string };
+
 export class Fetch {
   static BASE_URL: string = "http://localhost:8000/tasks";
+
   static async getAll() {
-    const response: Response = await fetch("http://localhost:8000/tasks");
+    const response: Response = await fetch(this.BASE_URL);
     if (!response.ok) {
       throw new Error(
         `Error al obtener las tareas: ${response.status} ${response.statusText}`
       );
     }
-    const data = await response.json();
+    const data: Data = await response.json();
     return data;
   }
 
-  static async create(task: Task) {
-    const response = await fetch(this.BASE_URL, {
+  static async create(task: ITask) {
+    const response: Response = await fetch(this.BASE_URL, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -29,11 +47,11 @@ export class Fetch {
         `Error al crear la tarea: ${response.status} ${response.statusText}`
       );
     }
-    const data = await response.json();
+    const data: ITask = await response.json();
     return data;
   }
 
-  static async update(task: Task) {
+  static async update(task: PatchTask) {
     const response = await fetch(`${this.BASE_URL}${task.id}`, {
       method: "PATCH",
       headers: {
@@ -46,7 +64,7 @@ export class Fetch {
         `Error al actualizar la tarea: ${response.status} ${response.statusText}`
       );
     }
-    const data = await response.json();
+    const data: ITask = await response.json();
     return data;
   }
 
